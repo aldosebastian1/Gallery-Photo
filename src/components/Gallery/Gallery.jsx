e0739af04eba15d376e648d385b6248eed96d8d0
Aldo Sebastian <aldoff23@gmail.com>
2025-10-14 15:27:11 +0700

    first initialization

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Gallery.css";
+
+function Gallery() {
+  return <div>Gallery</div>;
+}
+
+export default Gallery;
---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ImageCard.css";
+
+function ImageCard() {
+  return <div>ImageCard</div>;
+}
+
+export default ImageCard;
