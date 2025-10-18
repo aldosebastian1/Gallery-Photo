ed4e45a9ab61102a0a01ce1805b9177e1187b074
akn <akn123456@users.noreply.github.com>
2025-10-18 19:24:33 +0700

    Membuat data gambar untuk Home

---
+++ src/assets/homeImageData.js
@@ -0,0 +1,61 @@
+// Data gambar untuk halaman Home
+
+import me01_art_highlights_169_en from '../assets/me01-art-highlights-169-en.png';
+import Pikachu from '../assets/Pikachu.png';
+import pokemon_center_169 from '../assets/pokemon-center-169.png';
+import pokemon_go_169 from '../assets/pokemon-go-169.png';
+import pokemon_legends_z_a_169_en from '../assets/pokemon-legends-z-a-169-en.png';
+import pokemon_legends_z_a_169_en_1 from '../assets/pokemon-legends-z-a-169-en (1).png';
+import pokemon_legends_z_a_169_en_2 from '../assets/pokemon-legends-z-a-169-en (2).png';
+import pokemon_legends_z_a_169_en_3 from '../assets/pokemon-legends-z-a-169-en (3).png';
+
+export const homeImages = [
+  {
+    id: '1',
+    name: 'me01-art-highlights-169-en.png',
+    image: me01_art_highlights_169_en,
+    description: 'Gambar promosi untuk konten halaman Home.'
+  },
+  {
+    id: '2',
+    name: 'Pikachu.png',
+    image: Pikachu,
+    description: 'Gambar promosi untuk konten halaman Home.'
+  },
+  {
+    id: '3',
+    name: 'pokemon-center-169.png',
+    image: pokemon_center_169,
+    description: 'Gambar promosi untuk konten halaman Home.'
+  },
+  {
+    id: '4',
+    name: 'pokemon-go-169.png',
+    image: pokemon_go_169,
+    description: 'Gambar promosi untuk konten halaman Home.'
+  },
+  {
+    id: '5',
+    name: 'pokemon-legends-z-a-169-en.png',
+    image: pokemon_legends_z_a_169_en,
+    description: 'Gambar promosi untuk konten halaman Home.'
+  },
+  {
+    id: '6',
+    name: 'pokemon-legends-z-a-169-en (1).png',
+    image: pokemon_legends_z_a_169_en_1,
+    description: 'Gambar promosi untuk konten halaman Home.'
+  },
+  {
+    id: '7',
+    name: 'pokemon-legends-z-a-169-en (2).png',
+    image: pokemon_legends_z_a_169_en_2,
+    description: 'Gambar promosi untuk konten halaman Home.'
+  },
+  {
+    id: '8',
+    name: 'pokemon-legends-z-a-169-en (3).png',
+    image: pokemon_legends_z_a_169_en_3,
+    description: 'Gambar promosi untuk konten halaman Home.'
+  },
+]
