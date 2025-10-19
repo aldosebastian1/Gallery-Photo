70527adbd4286e89d3d01ce3967891fbd48f340a
Aldo Sebastian <aldoff23@gmail.com>
2025-10-19 14:26:54 +0700

    create dashboard list, and create styling dashboardlist, and update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Dashboard from './Gallery/Page/Dashboard';
 import GalleryPage from "./Gallery/Page/GalleryPage";
 import ImageDetail from "./Gallery/Page/ImageDetail";
-// import Home from "./Gallery/Page/Home";
 import Pokedex from "./Gallery/Page/Pokedex";
-import "./index.css";
 import Navbar from "./Gallery/components/Navbar/Navbar";
+import DashboardList from "./Gallery/Page/Dashboard";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<DashboardList />} />
         <Route path="/pokedex" element={<Pokedex />} />
         <Route path="/galeri" element={<GalleryPage />} />
         <Route path="/detail/:id" element={<ImageDetail />} />
