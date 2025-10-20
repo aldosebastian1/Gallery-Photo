70ef5f77c0f5be2ff82db8941cd74e05d97f59cc
Aldo Sebastian <aldoff23@gmail.com>
2025-10-20 13:01:40 +0700

    backup: pre-deploy-fix files

---
+++ .backups/deploy-fix-20251020/App.jsx
@@ -0,0 +1,24 @@
+// backup of src/App.jsx before deploy fixes (2025-10-20)
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GalleryPage from "./Gallery/Page/GalleryPage";
+import ImageDetail from "./Gallery/Page/ImageDetail";
+import Pokedex from "./Gallery/Page/Pokedex";
+import Navbar from "./Gallery/components/Navbar/Navbar";
+import DashboardDisplay from "./Gallery/Page/DashboardDisplay";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<DashboardDisplay />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+        <Route path="/galeri" element={<GalleryPage />} />
+        <Route path="/detail/:id" element={<ImageDetail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
