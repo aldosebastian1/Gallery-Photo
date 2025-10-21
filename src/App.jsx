8b8821c20331bb84e700f3de2e4e956e1fa53feb
Aldo Sebastian <aldoff23@gmail.com>
2025-10-22 00:55:29 +0700

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import GalleryPage from "./Gallery/Page/GalleryPage";
-import ImageDetail from "./Gallery/Page/ImageDetail";
 import Pokedex from "./Gallery/Page/Pokedex";
 import Navbar from "./Gallery/components/Navbar/Navbar";
 import DashboardDisplay from "./Gallery/Page/DashboardDisplay";
@@ -13,8 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<DashboardDisplay />} />
         <Route path="/pokedex" element={<Pokedex />} />
-        <Route path="/galeri" element={<GalleryPage />} />
-        <Route path="/detail/:id" element={<ImageDetail />} />
       </Routes>
     </Router>
   );
